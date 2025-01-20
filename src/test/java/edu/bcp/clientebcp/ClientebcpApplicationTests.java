7e65a81c75299f0259860065e3cf148c67e8d34e
joan157 <jphuauya@hotmail.com>
2025-01-19 23:22:45 -0500

    modificado el test

--- src/test/java/edu/bcp/clientebcp/ClientebcpApplicationTests.java
+++ src/test/java/edu/bcp/clientebcp/ClientebcpApplicationTests.java
@@ -3,7 +3,9 @@ package edu.bcp.clientebcp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import edu.bcp.config.AppConfig;
+
+@SpringBootTest(classes = AppConfig.class)
 class ClientebcpApplicationTests {
 
 	@Test
